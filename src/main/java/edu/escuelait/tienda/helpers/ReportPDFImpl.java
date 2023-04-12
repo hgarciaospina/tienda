93adaa64109adbb3a2ad3348b02269fc91c39bc6
hgarciaospina <henrygarciaospina@gmail.com>
2023-04-12 13:52:38 -0500

    Creación de estereotipo customizado @Report que reemplaza @Component

--- src/main/java/edu/escuelait/tienda/helpers/ReportPDFImpl.java
+++ src/main/java/edu/escuelait/tienda/helpers/ReportPDFImpl.java
@@ -1,16 +1,18 @@
 package edu.escuelait.tienda.helpers;
 
 import edu.escuelait.tienda.domain.Persona;
+import edu.escuelait.tienda.stereotypes.Report;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
-@Component
+@Report
 public class ReportPDFImpl {
 
     public void generatePdfReport(List<Persona> personas){
+
         log.info("Generate PDF Report {}", personas);
     }
 }
